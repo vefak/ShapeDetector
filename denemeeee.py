f3c6552dbb61631a729c515073119c29f6924fd0
vefak <vmakman@gmail.com>
2020-12-23 02:14:00 +0300

    shapeDetermine method add

--- denemeeee.py
+++ denemeeee.py
@@ -12,7 +12,30 @@ import math
 import matplotlib.pyplot as plt
 
 
+def dot(vA, vB):
+    return vA[0]*vB[0]+vA[1]*vB[1]
+def ang(lineA, lineB):
+    # Get nicer vector form
+    vA = [(lineA[0][0]-lineA[1][0]), (lineA[0][1]-lineA[1][1])]
+    vB = [(lineB[0][0]-lineB[1][0]), (lineB[0][1]-lineB[1][1])]
+    # Get dot prod
+    dot_prod = dot(vA, vB)
+    # Get magnitudes
+    magA = dot(vA, vA)**0.5
+    magB = dot(vB, vB)**0.5
+    # Get angle in radians and then convert to degrees
+    angle = math.acos(dot_prod/magB/magA)
+    # Basically doing angle <- angle mod 360
+    ang_deg = math.degrees(angle)%360
 
+    if ang_deg-180>=0:
+        # As in if statement
+        return 360 - ang_deg
+    else: 
+
+        return ang_deg
+
+    
 def _perpendicular_distance(p, p1, p2):
     dx = p2[0] - p1[0]
     dy = p2[1] - p1[1]
@@ -64,17 +87,27 @@ def RDP(line,epsilon):
         return results
 
 img  = cv2.imread("./shapes.bmp")
+img2 = cv2.imread("./shapes.bmp")
 plt.imshow(img)
 
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-blur = cv2.GaussianBlur(gray, (3,3),0)
+blur = cv2.GaussianBlur(gray, (7,17),0)
 
 _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
 
-plt.imshow(thresh)
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(7,7),(3,3))
 
-_, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+dilation = cv2.dilate(thresh,kernel,iterations = 1)
+#cv2.imwrite('square-cirlce-1_D.jpg',dilation)
+erosion = cv2.erode(dilation,kernel,iterations = 1)
+#cv2.imwrite('square-cirlce-1_E.jpg',erosion)
+#dilation_s = cv2.dilate(erosion,kernel,iterations = 1)
+
+
+plt.imshow(erosion)
+
+_, contours, _ = cv2.findContours(erosion, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
 print(len(contours))
 
@@ -86,22 +119,50 @@ for c in contours:
 
 print(len(filtered))
 
-objects = np.zeros([img.shape[0],img.shape[1],3], 'uint8')
 
-points = []
+
+def shapeDetermine(angs,contour):    
+    if  any(x > 57 and x < 63 for x in angs):
+        cv2.drawContours(img, [contour], 0, (255, 255, 0), -1)
+        
+    elif  any(x > 87 and x < 93 for x in angs):
+        cv2.drawContours(img, [contour], 0, (0, 0, 200), -1)
     
-for c in filtered:
-    p = cv2.arcLength(c,True)
-    res = RDP(c[:,0,:],p*0.01)
-    points.append(res)
-    color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
-    for i in range(len(res)-1):
-        cv2.line(img, (res[i][0],res[i][1]), (res[i+1][0],res[i+1][1]), color, 10)
+    elif  any(x > 102 and x < 109 for x in angs):
+        cv2.drawContours(img, [contour], 0, (255,105,180), -1)
+        
+    elif  any(x > 117 and x < 123 for x in angs):
+        cv2.drawContours(img, [contour], 0,(255,69,0), -1)
+    else:
+        cv2.drawContours(img, [contour], 0,(0,200,0), -1)
+
+  
 
 
+    
+numberedges = []
 
+for j, cnt in enumerate(filtered):
+    angles = []
+    p = cv2.arcLength(cnt,True)
+    res = RDP(cnt[:,0,:],p*0.01)    
+    print("New Shape {}\n".format(j))
+    print("Angles")
+    numberedges.append(res)
+    for i in range(len(res)-2):
+        line1 = (res[i], res[i+1])
+        line2 = (res[i+2], res[i+1])
+        angles.append(ang(line1,line2))
+        cv2.putText(img,"{}".format(j),(res[0][0],res[0][1]),cv2.FONT_HERSHEY_TRIPLEX,1,(200,200,200),3)
+        print(ang(line1,line2))
+    shapeDetermine(angles,cnt)
+    print("\n")
+    j +=1  
 plt.imshow(img)
 
+        
+        
+        
 
 
 
